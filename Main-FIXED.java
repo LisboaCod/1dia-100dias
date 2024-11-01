7bfba606e051f539bb239f3ff135f71ee0057662
LisboaCod <LisboaCod@hotmail.com>
2024-11-01 11:48:40 -0300

    FIX - Correção pois estava tudo comentado e o arquivo em branco.

---
+++ Main-FIXED.java
@@ -0,0 +1,45 @@
+// Mostra como fazer um Hello World
+public class Main
+{
+    public static void main(String args[])
+    {
+        System.out.println("Hello World");
+    }
+}
+
+// Aqui temos as váriaveis de tipo Inteiro
+
+public class VariaveisTipoInt
+{
+    public static void main(String args[])
+    {
+        System.out.println("Variavéis de tipo inteiro 1,2,3.-1,,2");
+    }
+}
+
+// Aqui temos as váriaveis tipo float
+public class VariaveisDeTipoFloat
+{
+    public static void main(String args[])
+    {
+        System.out.println("Aqui temos os tipo float números com virgula, 1.2, 1.3, sempre usar o f")
+    }
+}
+
+//Aqui temos O char
+public class VariaveisDeTipoChar
+{
+    public static void main(String args[])
+    {
+        System.out.println("Aqui temos o CHAR, váriavel que armazena apenas uma letra.");
+    }
+}
+
+// Aqui temos variaveis string
+public class VariavelTipoString
+{
+    public static void main()
+    {
+        System.out.println("Variaveis tipo string guardaria o nick por exemplo");
+    }
+}
